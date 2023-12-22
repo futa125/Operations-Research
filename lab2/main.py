96bbf3272f6b13dfea3363c3cb2f0bc94b3a430f
futa125 <ivanfutivic@gmail.com>
2023-12-22 21:07:50 +0100

    Add lab2

---
+++ lab2/main.py
@@ -0,0 +1,44 @@
+from typing import Any, Dict
+
+import numpy as np
+import numpy.typing as npt
+from pulp import LpVariable, LpProblem, LpMinimize, lpSum, LpBinary, PULP_CBC_CMD, PULP_CHOCO_CMD
+
+
+def construct_problem(costs: npt.NDArray[np.int64]) -> LpProblem:
+    rows: int
+    columns: int
+    rows, columns = costs.shape
+
+    x: Dict[Any, Dict[Any, LpVariable]] = LpVariable.dicts(name="x", indexs=(range(rows), range(columns)), cat=LpBinary)
+
+    problem: LpProblem = LpProblem("assignment_problem", LpMinimize)
+    problem += lpSum(costs[i][j] * x[i][j] for i in range(rows) for j in range(columns))
+
+    for i in range(rows):
+        problem += lpSum(x[i][j] for j in range(columns)) == 1
+
+    for j in range(columns):
+        problem += lpSum(x[i][j] for i in range(rows)) == 1
+
+    return problem
+
+
+def solve_LP(problem: LpProblem) -> LpProblem:
+    problem.solver = PULP_CBC_CMD(msg=False)
+    problem.solve()
+
+    return problem
+
+
+def solve_CP(problem: LpProblem) -> LpProblem:
+    problem.solver = PULP_CHOCO_CMD(msg=False)
+    problem.solve()
+
+    return problem
+
+
+np.random.seed(3652249321)
+
+mali_costs: npt.NDArray[np.int64] = np.random.randint(low=1, high=20 + 1, size=(8, 8), dtype=np.int64)
+veliki_costs: npt.NDArray[np.int64] = np.random.randint(low=1, high=20 + 1, size=(16, 16), dtype=np.int64)
