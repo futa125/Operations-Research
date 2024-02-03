2ae0292105fabf7af87e176602df26e6c662ef46
futa125 <ivanfutivic@gmail.com>
2024-02-03 22:02:28 +0100

    fix lab3

--- lab3/main.py
+++ lab3/main.py
@@ -1,29 +1,29 @@
-from typing import Dict, List, Tuple
+from typing import Dict, Tuple
 
-from pulp import LpMaximize, LpProblem, LpVariable, LpInteger
+import numpy as np
+from pulp import LpMaximize, LpProblem, LpVariable, lpSum
 
 
 def construct_LP_equivalent() -> Tuple[LpProblem, Dict[str, LpVariable]]:
     problem: LpProblem = LpProblem(name="robust-linear-program", sense=LpMaximize)
 
-    x1: LpVariable = LpVariable(name="x1", lowBound=0, upBound=4, cat=LpInteger)
-    x2: LpVariable = LpVariable(name="x2", lowBound=0, upBound=6, cat=LpInteger)
+    x1: LpVariable = LpVariable(name="x1", lowBound=0, upBound=4)
+    x2: LpVariable = LpVariable(name="x2", lowBound=0, upBound=6)
 
     variables: Dict[str, LpVariable] = {
-        x1.name: x1,
-        x2.name: x2,
+        "x1": x1,
+        "x2": x2,
     }
+
+    ud = 4
+    p1 = LpVariable.dicts(name="p1", indexs=range(ud), lowBound=0)
+    d = np.array([0, 2, -4, 6]).T
+    d1 = np.array([[-1, 1], [1, -1], [-1, -1], [1, 1]])
 
     problem += 3 * x1 + 5 * x2
 
-    max_alpha: int = 6
-    max_beta: int = 6
-    points: List[Tuple[int, int]] = [
-        (alpha, beta) for alpha in range(max_alpha + 1) for beta in range(max_beta + 1)
-        if 0 <= alpha - beta <= 2 and 4 <= alpha + beta <= 6
-    ]
-
-    for alpha, beta in points:
-        problem += alpha * x1 + beta * x2 <= 18
+    problem += lpSum([p1[i] * d[i] for i in range(ud)]) <= 18
+    problem += lpSum([p1[i] * d1[i, 0] for i in range(ud)]) == x1
+    problem += lpSum([p1[i] * d1[i, 1] for i in range(ud)]) == x2
 
     return problem, variables
